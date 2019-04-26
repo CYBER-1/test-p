9f3446c7d75d4beef3ab3201f3b66b04cc6727b7
Mykola Mykhailian <mnykolay@gmail.com>
2019-04-26 16:50:36 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-$(function() {
+/*$(function() {
     $('a[href*=#]').on('click', function(e) {
       e.preventDefault();
       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
   });
+  
   
 
 $(function () {
@@ -33,7 +34,7 @@ $(function() {
     });
   });
 });
-
+*/
 /* Slides */
 
 'use strict';
