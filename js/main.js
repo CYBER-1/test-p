b347f07df51d19c7c89ecbac4e49165d56a4ae4f
Mykola Mykhailian <mnykolay@gmail.com>
2019-05-12 17:45:55 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
-'use strict';$(function(){ var width='100vh'; var animationSpeed=1500; var pause=20000; var currentSlide=1; var $slider=$('#cs1'); var $slideContainer=$('.slides', $slider); var $slides=$('.slide', $slider); var interval; function startSlider(){interval=setInterval(slide, pause);}function slide(){$slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){if (++currentSlide===$slides.length){currentSlide=1; $slideContainer.css('margin-left', 0);}});}function pauseSlider(){clearInterval(interval);}$slideContainer .on('mouseenter', pauseSlider) .on('mouseleave', startSlider) .on('click', slide); startSlider();});
+
+'use strict';
+
+$(function() {
+
+  //settings for slider
+  var width = '100vh';
+  var animationSpeed = 1500;
+  var pause = 20000;
+  var currentSlide = 1;
+
+  //cache DOM elements
+  var $slider = $('#cs1');
+  var $slideContainer = $('.slides', $slider);
+  var $slides = $('.slide', $slider);
+
+  var interval;
+
+  function startSlider() {
+    interval = setInterval(slide, pause);
+  }
+
+  function slide() {
+    $slideContainer.animate({
+      'margin-left': '-=' + width
+    }, animationSpeed, function() {
+      if (++currentSlide === $slides.length) {
+        currentSlide = 1;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+  }
+
+  function pauseSlider() {
+    clearInterval(interval);
+  }
+
+  $slideContainer
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider)
+    .on('click', slide);
+
+  startSlider();
+
+
+});
   
 
   
